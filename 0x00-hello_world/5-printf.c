ad1627bbcc09d3d7c26dba74468e043dc47e5967
David-0102 <davidmumondunda@gmail.com>
2023-09-12 02:59:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * program printing output using printf
+ *
+ * Author- Mumo Ndunda
+ */
+
+/*
+ * main - Entry pointfor the program
+ *
+ * Return: 0 success
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
