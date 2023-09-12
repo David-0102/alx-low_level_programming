5bc62cee237f9c5ea16abe844ced34c02e9945f7
David-0102 <davidmumondunda@gmail.com>
2023-09-12 02:20:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+
+/*
+ * C program that prints string using puts
+ * Author- Mumo Ndunda
+ *
+ */
+
+#include <stdio.h>
+
+/* Main - entry point
+ *
+ * return- 0 success
+ */
+
+int main(void)
+{
+	char str[] == "Programming is like building a multilingual puzzle,\n";
+	puts(str);
+
+	return (0);
+}
