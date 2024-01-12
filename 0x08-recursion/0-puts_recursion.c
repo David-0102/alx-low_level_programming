9b6219de84c2fd886eef8cc4b6a75a7ff282d7bc
David-0102 <davidmumondunda@gmail.com>
2024-01-12 05:04:38 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /*
  * recursion program that prints a string followed by a new line
- *@s: input string
  * Author: Mumo Ndunda
  */
 
-/*
- * main - entry point for the program
+/**
  * _puts_recursion - Prints a string followed by a newline using recursive algo
+ * @s: input string
  * return: 1 On success
  * On error, return -1 and set errno appropriately
  */
